7facdc7d99107bc109ba0047c51332b7de60b242
AndreaGasparMiramontes <142550697+AndreaGasparMiramontes@users.noreply.github.com>
2023-11-04 15:32:51 -0600

    mini analizador sintactico

--- Analizador sintactico (objetos)/main.py
+++ Analizador sintactico (objetos)/main.py
@@ -1,23 +1,97 @@
 import lexico
+from pila import Pila
+from ep import EP
 
 ##    | id |  + |  $ |  E |
 ##  0 | d2 |    |    |  1 |
 ##  1 |    |    | r0 |    |
 ##  2 |    | d3 | r2 |    |
-##  3 | d2 |    |    |    |
+##  3 | d2 |    |    |  4 |
 ##  4 |    |    | r1 |    |
-arreglo = [[2,0,0,1],[0,0,-1,0],[0,3,-3,0],[0,2,0,0],[0,0,-2,0]]
 
 estado = 0
 
+pila = Pila()
+
 texto = input("Introduce el texto a analizar: ")
 elementos = lexico.analizador(texto)
-for elemento in elementos:
-    print("Lexema: ",elemento['lexema'],"Token: ",elemento['token'],"num: ",elemento['num'])
+e = False
+accepted = False
+pila.push(0)
+print(pila)
+a = 0
+while(not accepted):
+    input()
+    estado = pila.top()
+    if isinstance(estado, EP):
+        e_cont = pila.pop()
+        e = True
+        estado = pila.top()
+    #print("Lexema: ",elementos[a]['lexema'],"Token: ",elementos[a]['token'],"num: ",elementos[a]['num'])
     if estado == 0:
-        if elemento['token'] == "id":
-            #agregar a pila el elemento y el numero 2
-            pass
-        elif elemento['token'] == "E":
-            #agregar a pila el elemento y el numero 2
-            pass
+        if e:
+            pila.push(e_cont)
+            pila.push(1)
+            e = False
+        elif elementos[a]['token'] == "id":
+            pila.push(elementos[a]['lexema'])
+            pila.push(2)
+            a+=1
+        else:
+            print("Cadena rechazada")
+            accepted = True
+        
+    if estado == 1:
+        if elementos[a]['token'] == "pesos":
+            pila.pop()
+            b = pila.pop()
+            print("Cadena aceptada")
+            print(b)
+            accepted = True
+        else:
+            print("Cadena rechazada")
+            accepted = True
+    if estado == 2:
+        if elementos[a]['token'] == "OpSuma":
+            pila.push(elementos[a]['lexema'])
+            pila.push(3)
+            a+=1
+        elif elementos[a]['token'] == "pesos":
+            pila.pop() #num
+            c = EP("E")
+            c.id = pila.pop() #id
+            pila.push(c)
+        else:
+            print("Cadena rechazada")
+            accepted = True
+    if estado == 3:
+        if e:
+            pila.push(e_cont)
+            pila.push(4)
+            e = False
+        elif elementos[a]['token'] == "id":
+            pila.push(elementos[a]['lexema'])
+            pila.push(2)
+            a+=1
+        else:
+            print("Cadena rechazada")
+            accepted = True
+        
+    if estado == 4:
+        if elementos[a]['token'] == "pesos":
+            c = EP("E")
+            pila.pop()
+            c.E = pila.pop()
+            pila.pop()
+            c.mas = pila.pop()
+            pila.pop()
+            c.id = pila.pop()
+            pila.push(c)
+        else:
+            print("Cadena rechazada")
+            accepted = True
+    print(pila)
+
+
+
+
